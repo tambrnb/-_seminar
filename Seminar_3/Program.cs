aac07097d4363bed1d5b60019a33453f14f50f5c
Tamerlan <t.boranbayev@yandex.ru>
2022-10-22 11:50:36 +0600

    Added Seminar_3

---
+++ Seminar_3/Program.cs
@@ -0,0 +1,91 @@
+﻿// 17. Напишите программу, 
+// которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, 
+// // в которой находится эта точка.
+
+// Console.WriteLine("Введите число");
+// int x = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int y = int.Parse(Console.ReadLine());
+
+// if(x>0&&y>0)
+// {
+//     Console.WriteLine("1");
+// }
+// if(x>0&&y<0)
+// {
+//   Console.WriteLine("4");  
+// }
+// if(x<0&&y>0)
+// {
+//     Console.WriteLine("2");
+// }
+// if(x<0&&y<0)
+// {
+//     Console.WriteLine("3");
+// }
+
+// Задача 21: Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+// Console.WriteLine("Введите число");
+// int x1 = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int y1 = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int x2 = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int y2 = int.Parse(Console.ReadLine());
+
+// double d = Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2));
+
+// Console.WriteLine(d);
+
+// ///////////////////
+// Задача 18: Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+// Console.WriteLine("Введите число");
+// int  num = int.Parse(Console.ReadLine());
+
+// if(num==1)
+// {
+// Console.WriteLine("x>0, y>0");
+// }
+
+// if(num==2)
+// {
+// Console.WriteLine("x<0, y>0");
+// }
+
+// if(num==3)
+// {
+// Console.WriteLine("x<0, y<0");
+// }
+
+// if(num==4)
+// {
+// Console.WriteLine("x>0, y<0");
+// }
+
+
+////////////////////////////////
+// Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+// Console.WriteLine("Введите число");
+// int  N = int.Parse(Console.ReadLine());
+// for(int i = 1; i<=N;i++)
+// {
+//     int PW = i*i;
+//     Console.WriteLine(PW);
+// }
+
+// //////////////
+// Домашняя работа/////////
